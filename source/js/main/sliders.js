b57c12dc32ec49a91ac153c1ed692be2f6845560
dunewalkin <sergei.gunchenko@gmail.com>
2025-02-22 01:27:34 +0400

    complete desktop about-page

--- source/js/main/sliders.js
+++ source/js/main/sliders.js
@@ -13,7 +13,6 @@
     const prevSelector = `${id} .swiper-prev`;
 
     new Swiper(elementSelector, {
-      // Optional parameters
       loop: true,
       slidesPerView: 1,
       speed: 1000,
@@ -121,6 +120,18 @@
       prevEl: ".js-news-slider .swiper-prev",
     },
   });
+
+  new Swiper(".js-teams-slider-container", {
+      loop: false,
+      slidesPerView: 3,
+      speed: 1000,
+      spaceBetween: 40,
+      pagination: false,
+      navigation: {
+      nextEl: ".js-teams-slider .swiper-next",
+      prevEl: ".js-teams-slider .swiper-prev",
+      },
+   });
 
 
 })();
