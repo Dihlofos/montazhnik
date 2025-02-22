eea6f931ca39717f41bc787ca6e4f2a4048bc478
dunewalkin <sergei.gunchenko@gmail.com>
2025-02-23 01:48:01 +0400

    complete services-page adaptive

--- build/js/main.js
+++ build/js/main.js
@@ -247,8 +247,16 @@
          nextEl: nextSelector,
          prevEl: prevSelector,
       },
+      breakpoints: {
+         0: { slidesPerView: 1 },  
+         768: { slidesPerView: 3 }, 
+         1025: { slidesPerView: 4 } 
+      }
    });
+   
    });
+
+  
 
 
    const standartsSliders = document.querySelectorAll(".js-standarts-slider");
@@ -259,18 +267,21 @@
       const nextSelector = `${id} .swiper-next`;
       const prevSelector = `${id} .swiper-prev`;
 
-      if (vw < 1025) {
-         new Swiper(elementSelector, {
-            loop: false,
-            slidesPerView: 1,
-            speed: 1000,
-            spaceBetween: 40,
-            navigation: {
-               nextEl: nextSelector,
-               prevEl: prevSelector,
-            },
-         });
-      }
+      new Swiper(elementSelector, {
+         loop: false,
+         slidesPerView: 4,
+         speed: 1000,
+         spaceBetween: 40,
+         navigation: {
+            nextEl: nextSelector,
+            prevEl: prevSelector,
+         },
+         breakpoints: {
+            0: { slidesPerView: 1 },  
+            768: { slidesPerView: 3 }, 
+            1025: { slidesPerView: 4 } 
+         }
+      });
 
       
    });
