f0aaa2f94532f022864b9894224b9c440e82fd1a
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2025-02-28 21:40:52 +0300

    done feedback form

--- build/js/main.js
+++ build/js/main.js
@@ -102,6 +102,69 @@
 
 "use strict";
 (function () {
+  emailjs.init({
+    publicKey: "ilhHVNppMlaoqE4RL",
+  });
+
+  const form = document.querySelector(".js-form");
+  const formSubmitButton = document.querySelector(".js-form-submit");
+  const formDone = document.querySelector(".js-form-done");
+  const formWrong = document.querySelector(".js-form-wrong");
+  initEvents();
+
+  function initEvents() {
+    if (!form) return;
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      grecaptcha.ready(function () {
+        grecaptcha
+          .execute("6LczUdEaAAAAACImrHbKWiSSutDmsNCH1sC9zXbu", {
+            action: "submit",
+          })
+          .then(function (token) {
+            let res = {};
+            const formData = new FormData(form);
+
+            for (let [key, value] of formData.entries()) {
+              res[key] = value;
+            }
+
+            sendEmail(res);
+          })
+          .catch((error) => {
+            formWrong.classList.add("show");
+          });
+      });
+    });
+  }
+
+  function sendEmail({ name, phone, message }) {
+    formSubmitButton.setAttribute("disabled", true);
+    formSubmitButton.classList.add("loading");
+
+    const templateParams = {
+      name,
+      phone,
+      message,
+    };
+
+    emailjs.send("service_pz53umj", "template_clwmtl9", templateParams).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        form.classList.remove("show");
+        formDone.classList.add("show");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        form.classList.remove("show");
+        formWrong.classList.add("show");
+      }
+    );
+  }
+})();
+
+"use strict";
+(function () {
   const nav = document.querySelector(".js-nav");
   const toggler = document.querySelector(".js-nav-toggler");
   const closeButtons = document.querySelectorAll(".js-nav-close");
--- source/js/main/feedbackForm.js
+++ source/js/main/feedbackForm.js
@@ -1,5 +1,9 @@
 "use strict";
 (function () {
+  emailjs.init({
+    publicKey: "ilhHVNppMlaoqE4RL",
+  });
+
   const form = document.querySelector(".js-form");
   const formSubmitButton = document.querySelector(".js-form-submit");
   const formDone = document.querySelector(".js-form-done");
@@ -22,35 +26,37 @@
             for (let [key, value] of formData.entries()) {
               res[key] = value;
             }
-            console.log("all correct, but not do anything further");
-            // sendEmail(res);
+
+            sendEmail(res);
           })
           .catch((error) => {
-            formWrong.style.opaciy = 1;
+            formWrong.classList.add("show");
           });
       });
     });
   }
 
-  function sendEmail({ name, email, phone, message }) {
+  function sendEmail({ name, phone, message }) {
     formSubmitButton.setAttribute("disabled", true);
-    Email.send({
-      SecureToken: "e5954290-31df-4abf-b5aa-8bb1d285f13a",
-      To: "info@vektoruspeha.com",
-      From: "info@vektoruspeha.com",
-      Subject: `${name} sent you a message`,
-      Body: `
-        <p><h4>Здравствуйте, меня зовут: ${name}</h4></p>
-        <p><strong>Сообщение:</strong> ${message}</p>
-        <p><strong>Адрес электронной почты:</strong> ${email}</p>
-        <p><strong>Телефон:</strong> ${phone}</p>
-      `,
-    })
-      .then((message) => {
-        formDone.style.opacity = 1;
-      })
-      .catch((error) => {
-        formWrong.style.opaciy = 1;
-      });
+    formSubmitButton.classList.add("loading");
+
+    const templateParams = {
+      name,
+      phone,
+      message,
+    };
+
+    emailjs.send("service_pz53umj", "template_clwmtl9", templateParams).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        form.classList.remove("show");
+        formDone.classList.add("show");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        form.classList.remove("show");
+        formWrong.classList.add("show");
+      }
+    );
   }
 })();
